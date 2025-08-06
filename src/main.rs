5558def33260c9f4fe57724f64bd9b79a74ec4fa
Iksboi <130296326+Iksboi@users.noreply.github.com>
2025-08-06 21:33:49 +0000

    Added ls like functionality.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let path = {
+        let mut args = std::env::args();
+        args.nth(1).unwrap_or(".".to_string())
+    };
+    let metadata = std::fs::metadata(&path).expect("Given path is invalid");
+
+    if metadata.is_dir() {
+        let dir_content = std::fs::read_dir(path).expect("Shouldnt crash, but whatever !");
+        for entry in dir_content {
+            println!("{}", entry.unwrap().file_name().to_str().unwrap());
+        }
+    } else if metadata.is_file() {
+        let path = std::path::PathBuf::from(path);
+        println!("{}", path.file_name().unwrap().to_str().unwrap());
+    }
 }
